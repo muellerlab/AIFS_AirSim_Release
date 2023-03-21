74069b976ae2586f815e266c5cd6943cddf8413d
Teaya-Yang <teaya.yang@berkeley.edu>
2023-03-21 00:52:26 -0700

    Added auto image generation script

---
+++ Unity/UnityDemo/Assets/AirSimAssets/Scripts/AutoImageScript.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class AutoImageScript : MonoBehaviour
+{
+
+    private float nextPicTime = 0.0f;
+    public float period = 1.0f;
+    public int FileCounter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextPicTime) {
+            nextPicTime += period;
+            CamCapture(); 
+        }
+    }
+ 
+    void CamCapture()
+    {
+        Camera Cam = GetComponent<Camera>();
+ 
+        RenderTexture currentRT = RenderTexture.active;
+        RenderTexture.active = Cam.targetTexture;
+ 
+        Cam.Render();
+ 
+        Texture2D Image = new Texture2D(Cam.targetTexture.width, Cam.targetTexture.height);
+        Image.ReadPixels(new Rect(0, 0, Cam.targetTexture.width, Cam.targetTexture.height), 0, 0);
+        Image.Apply();
+        RenderTexture.active = currentRT;
+ 
+        var Bytes = Image.EncodeToPNG();
+        Destroy(Image);
+ 
+        File.WriteAllBytes(Application.dataPath + "/ImageSequence/" + FileCounter + ".png", Bytes);
+        FileCounter++;
+    }
+}
